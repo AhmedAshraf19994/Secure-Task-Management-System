210bccef3a37f8562f3b0fe80b06c0c1015a5264
Ahmed <holyhera0@gmail.com>
2026-04-27 10:25:45 +0300

    fix pom.xml edit config for plug in to allow mappers in compile time

--- src/main/java/com/ahmed/Secure/Task/Management/System/task/TaskMapper.java
+++ src/main/java/com/ahmed/Secure/Task/Management/System/task/TaskMapper.java
@@ -1,49 +1,4 @@
 package com.ahmed.Secure.Task.Management.System.task;//package com.ahmed.Secure.Task.Management.System.task;
-//
-//import com.ahmed.Secure.Task.Management.System.task.dto.CreateTaskDto;
-//import com.ahmed.Secure.Task.Management.System.task.dto.TaskResponseDto;
-//import com.ahmed.Secure.Task.Management.System.task.dto.UpdateTaskDto;
-//import com.ahmed.Secure.Task.Management.System.user.User;
-//import com.ahmed.Secure.Task.Management.System.user.UserMapper;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//@RequiredArgsConstructor
-//public class TaskMapper {
-//
-//    private final UserMapper userMapper;
-//
-//    public Task toTask (CreateTaskDto createTaskDto, User assignedTo) {
-//        return Task.builder()
-//                .title(createTaskDto.title())
-//                .description(createTaskDto.description())
-//                .dueDate(createTaskDto.dueDate())
-//                .assignedTo(assignedTo)
-//                .priority(createTaskDto.priority() == null ? TaskPriority.LOW : createTaskDto.priority())
-//                .build();
-//    }
-//
-//    public TaskResponseDto toTaskResponseDto (Task task) {
-//        return new TaskResponseDto(
-//                task.getId(),
-//                task.getTitle(),
-//                task.getDescription(),
-//                task.getDueDate(),
-//                task.getStatus(),
-//                task.getPriority(),
-//                this.userMapper.toUserResponseDto(task.getCreatedBy()),
-//                task.getAssignedTo() == null ? null : this.userMapper.toUserResponseDto(task.getAssignedTo())
-//
-//        );
-//    }
-//
-//    public Task fromUpdateTaskDtoToTask (UpdateTaskDto updateTaskDto) {
-//        return Task
-//                .builder()
-//                .title(updateTaskDto.title() !null )
-//
-//    }
 
 import com.ahmed.Secure.Task.Management.System.task.dto.CreateTaskDto;
 import com.ahmed.Secure.Task.Management.System.task.dto.TaskResponseDto;
